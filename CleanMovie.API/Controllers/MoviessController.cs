ecab579b7085abdd40cc1f035800366132a3605f
Vishal-Agarwal-Git <vishalagar86@gmail.com>
2026-05-25 23:39:40 +0530

    Add project files.

---
+++ CleanMovie.API/Controllers/MoviessController.cs
@@ -0,0 +1,29 @@
+﻿using CleanMovie.Application;
+using CleanMovie.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CleanMovie.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviessController : ControllerBase
+    {
+        private readonly IMovieService _service;
+
+        public MoviessController(IMovieService service)
+        {
+            _service = service;
+        }
+        // GET: api/<MoviessController>
+        [HttpGet]
+        public ActionResult<List<Movie>> Get()
+        {
+            var moviesFromService = _service.GetAllMovies();
+            return Ok(moviesFromService);
+        }
+
+        
+    }
+}
