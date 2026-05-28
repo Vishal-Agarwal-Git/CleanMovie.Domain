e0803d3efbde7f0b67d3f6d5a614e34ec6ae8fcc
Vishal-Agarwal-Git <vishalagar86@gmail.com>
2026-05-28 23:18:28 +0530

    Domain Updated

---
+++ CleanMovie.Domain/DefaulterList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanMovie.Domain
+{
+    public class DefaulterList
+    {
+        public int DefId { get; set; }
+        public int DefType { get; set; }
+        public int DefName { get; set; }
+    }
+}
