ceb9722d1ec44ec3b6f5c193c62951cd62c85adf
Vishal-Agarwal-Git <vishalagar86@gmail.com>
2026-06-03 23:43:53 +0530

    Movie Domain again Updated

--- CleanMovie.Domain/Movie.cs
+++ CleanMovie.Domain/Movie.cs
@@ -8,18 +8,20 @@ namespace CleanMovie.Domain
 {
     public class Movie
     {
-        public int MovieId { get; set; }
-        public string MovieName { get; set; } = string.Empty;
-        public decimal RentalCost { get; set; }
-        //   public int RentalDuration { get; set; }
+        //   public int MovieId { get; set; }
+        //   public string MovieName { get; set; } = string.Empty;
+        //   public decimal RentalCost { get; set; }
+        ////   public int RentalDuration { get; set; }
 
-           // Many to Many Relationship    
-        //   public IList<MovieRental> MovieRentals { get; set; }
-        
+        //   // Many to Many Relationship    
+        ////   public IList<MovieRental> MovieRentals { get; set; }
+        ///
 
 
 
-        
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal Cost { get; set; }
         
     }
 }
