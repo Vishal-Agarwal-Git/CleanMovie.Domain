e23677c6836703a7a29936e6a2e0ffa9e191fc1c
Vishal-Agarwal-Git <vishalagar86@gmail.com>
2026-06-06 23:48:25 +0530

    .

--- CleanMovie.Application/IMovieService.cs
+++ CleanMovie.Application/IMovieService.cs
@@ -13,6 +13,7 @@ namespace CleanMovie.Application
         List<Movie> GetAllMovies();
         Movie CreateMovie(Movie movie);
         Movie GetMovieById(int id);
+        Movie UpdateMovie(Movie movie);
         Movie Delete(int id);
     }
 }
